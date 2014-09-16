77de3b8ab5187c534d1375e1fc8820805dc14662
chrisjansky <c.jansky@de.edenspiekermann.com>
2014-09-16 20:08:17 +0200

    WIP: changed javascripts

--- assets/js/bzenia-paper.js
+++ assets/js/bzenia-paper.js
@@ -1,36 +1,38 @@
 var width, height, center;
-var points = 6;
+var points = 4;
 var path = new Path();
 var mousePos = view.center / 2;
 var pathHeight = mousePos.y;
 path.fillColor = 'white';
+// path.strokeColor = 'black';
+// path.strokeColor = '#97bf1d';
 initializePath();
 
 function initializePath() {
   center = view.center;
   width = view.size.width;
-  height = view.size.height * 0.625;
+  height = view.size.height * 0.5;
   path.segments = [];
   path.add(view.bounds.topLeft);
-  path.add(view.bounds.leftCenter);
+  path.add(0, view.size.height * 0.625);
   for (var i = 1; i < points; i++) {
     var point = new Point(width / points * i, center.y);
     path.add(point);
   }
-  path.add(view.bounds.rightCenter);
+  path.add(width, view.size.height * 0.125);
   path.add(view.bounds.topRight);
 }
 
-// function onFrame(event) {
-//   pathHeight += (center.y - mousePos.y / 5 - pathHeight) / 10;
-//   for (var i = 2; i < points; i++) {
-//     var sinSeed = event.count + (i + i % 10) * 100;
-//     var sinHeight = Math.sin(sinSeed / 200) * pathHeight;
-//     var yPos = Math.sin(sinSeed / 100) * sinHeight + height;
-//     path.segments[i].point.y = yPos;
-//   }
-//   path.smooth();
-// }
+function onFrame(event) {
+  pathHeight += (center.y - mousePos.y / 5 - pathHeight) / 150;
+  for (var i = 2; i < points; i++) {
+    var sinSeed = event.count + (i + i % 10) * 150;
+    var sinHeight = Math.sin(sinSeed / 50) * pathHeight;
+    var yPos = Math.sin(sinSeed / 100) * sinHeight + height;
+    path.segments[i].point.y = yPos;
+  }
+  path.smooth();
+}
 
 function onMouseMove(event) {
   mousePos = event.point;
--- assets/js/bzenia.js
+++ assets/js/bzenia.js
@@ -2,23 +2,30 @@
 
 // alert("lol");
 
-$('.m-brief')
-  .waypoint(function(direction) {
-    $("body").toggleClass("offscreen--not-active offscreen--is-active");
+// $('.m-brief')
+//   .waypoint(function(direction) {
+//     $("body").toggleClass("offscreen--not-active offscreen--is-active");
   // }, {
   //   offset: function() {
   //     return $(this).height() / 2;
   //   }
-  });
-$('.m-hero')
-  .waypoint(function(direction) {
-    $(this).toggleClass("is-active");
-  }, {
-    offset: function() {
-      return $(this).height() / 2;
-    }
-  });
+  // });
+// $('.m-hero')
+//   .waypoint(function(direction) {
+//     $(this).toggleClass("is-active");
+//   }, {
+//     offset: function() {
+//       return $(this).height() / 2;
+//     }
+//   });
 
+// $('.m-offscreen')
+//   .on("mouseenter", function() {
+//     $("body").addClass("content--is-offset");
+//   })
+//   .on("mouseleave", function() {
+//     $("body").removeClass("content--is-offset");
+//   });
 // function updateCanvas() {
   // console.log(ulozeno);
   // paperscript.makeBlack();
