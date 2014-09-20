3d0ac1fc63dd8987a0250110e65174f7bc13bbde
chrisjansky <c.jansky@de.edenspiekermann.com>
2014-09-21 01:14:26 +0200

    Moved all JS to .on("ready")

--- assets/js/bzenia.js
+++ assets/js/bzenia.js
@@ -1,35 +1,39 @@
-var
-  $body = $("body"),
-  $pjaxContainer = $("#js-pjax"),
-  // Prevent opening Featherlight lightbox when touching Swiper.
-  swiperActive = false;
+$(document).on("ready pjax:success", function() {
+  var
+    $body = $("body"),
+    $pjaxWhole = $("#js-pjax--whole"),
+    // Prevent opening Featherlight lightbox when touching Swiper.
+    swiperActive = false;
 
-// Set-up pjax.
-if ($.support.pjax) {
+  // Set-up pjax.
+  if ($.support.pjax) {
 
-  $body.addClass("is-ready");
+    $body.addClass("is-ready");
 
-  $(document).on("click", "[data-pjax]", function(event) {
-    
-    $.pjax.click(event, {
-      container: $pjaxContainer,
-      fragment: "#js-pjax"
+    $(document).on("click", "[data-pjax--whole]", function(event) {
+      $.pjax.click(event, {
+        container: $pjaxWhole,
+        fragment: "#js-pjax--whole"
+      });
     });
 
-  });
+    $(document).on("click", "[data-pjax--aside]", function(event) {
+      $.pjax.click(event, {
+        container: $("#js-pjax--aside"),
+        fragment: "#js-pjax--main"
+      });
+    });
 
-  $pjaxContainer.on({
-    'pjax:send': function() {
-      $body.addClass("is-loading");
-    },
-    'pjax:complete': function() {
-      $body.removeClass("is-loading");
-    }
-  });
+    $pjaxWhole.on({
+      'pjax:send': function() {
+        $body.addClass("is-loading");
+      },
+      'pjax:complete': function() {
+        $body.removeClass("is-loading");
+      }
+    });
 
-}
-
-$(document).on("ready pjax:success", function() {
+  }
 
   // Initialize Swiper.
   var gallerySwiper = new Swiper('.swiper-container', {
