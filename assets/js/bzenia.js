50fecdc87f09e1c74f0776e19aff84044552f3e8
chrisjansky <c.jansky@de.edenspiekermann.com>
2014-09-20 10:43:19 +0200

    Implemented featherlight lightbox

--- assets/js/bzenia.js
+++ assets/js/bzenia.js
@@ -1,35 +1,27 @@
+// Prevent opening Featherlight lightbox when touching Swiper.
+var swiperActive = false;
+
+// Initialize Swiper.
 var gallerySwiper = new Swiper('.swiper-container', {
   slideElement: "li",
   slidesPerView: "auto",
+  onTouchMove: function() {
+    swiperActive = true;
+  },
+  onTouchEnd: function() {
+    setTimeout(function() {
+      swiperActive = false;
+    }, 250);
+  }
 });
 
-// $('.m-brief')
-//   .waypoint(function(direction) {
-//     $("body").toggleClass("offscreen--not-active offscreen--is-active");
-  // }, {
-  //   offset: function() {
-  //     return $(this).height() / 2;
-  //   }
-  // });
-// $('.m-hero')
-//   .waypoint(function(direction) {
-//     $(this).toggleClass("is-active");
-//   }, {
-//     offset: function() {
-//       return $(this).height() / 2;
-//     }
-//   });
-
-// $('.m-offscreen')
-//   .on("mouseenter", function() {
-//     $("body").addClass("content--is-offset");
-//   })
-//   .on("mouseleave", function() {
-//     $("body").removeClass("content--is-offset");
-//   });
-// function updateCanvas() {
-  // console.log(ulozeno);
-  // paperscript.makeBlack();
-  // ulozeno.view.path.fillColor = "yellow";
-  // return false;
-// }
+// Trigger Featherlight lightbox on click.
+$("[data-js-featherlight]").on("click", function(e) {
+  e.preventDefault();
+  if (!swiperActive) {
+    $.featherlight(this.href, {
+      closeOnClick: "anywhere",
+      type: {image: true}
+    });
+  }
+});
